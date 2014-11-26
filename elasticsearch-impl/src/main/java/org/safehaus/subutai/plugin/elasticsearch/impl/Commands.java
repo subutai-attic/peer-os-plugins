076c5c0fd2b1ed62a0b811753a8d039c5e9607bf
Ebru Cetinkaya <ecetinkaya@safehaus.org>
2014-11-26 17:51:04 +0200

    separate elasticsearch-plugin

---
+++ elasticsearch-impl/src/main/java/org/safehaus/subutai/plugin/elasticsearch/impl/Commands.java
@@ -0,0 +1,10 @@
+package org.safehaus.subutai.plugin.elasticsearch.impl;
+
+
+public class Commands
+{
+    public static String statusCommand = "service elasticsearch status";
+    public static String startCommand = "service elasticsearch start";
+    public static String stopCommand = "service elasticsearch stop";
+    public static String configure = ". /etc/profile && es-conf.sh";
+}
