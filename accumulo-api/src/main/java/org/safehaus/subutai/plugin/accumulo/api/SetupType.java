2fb5670721f4dc216debcceba63c1fc04e543df4
Ebru Cetinkaya <ecetinkaya@safehaus.org>
2014-11-26 17:38:09 +0200

    separate accumulo-plugin

---
+++ accumulo-api/src/main/java/org/safehaus/subutai/plugin/accumulo/api/SetupType.java
@@ -0,0 +1,7 @@
+package org.safehaus.subutai.plugin.accumulo.api;
+
+
+public enum SetupType
+{
+    OVER_HADOOP_N_ZK, WITH_HADOOP_N_ZK
+}
---
+++ accumulo-ui/src/main/java/org/safehaus/subutai/plugin/accumulo/ui/manager/CompleteEvent.java
@@ -0,0 +1,7 @@
+package org.safehaus.subutai.plugin.accumulo.ui.manager;
+
+
+public interface CompleteEvent
+{
+    public void onComplete( String status );
+}
