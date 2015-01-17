140525bcf94bba09a3420420d3f421824ea91de8
Ebru Cetinkaya <ecetinkaya@safehaus.org>
2015-01-17 16:59:31 +0200

    fix destroy-cluster

--- cassandra-ui/src/main/java/org/safehaus/subutai/plugin/cassandra/ui/manager/Manager.java
+++ cassandra-ui/src/main/java/org/safehaus/subutai/plugin/cassandra/ui/manager/Manager.java
@@ -346,6 +346,7 @@ public class Manager
                                 public void windowClose( Window.CloseEvent closeEvent )
                                 {
                                     refreshClustersInfo();
+                                    refreshUI();
                                 }
                             } );
                             contentRoot.getUI().addWindow( window.getWindow() );
