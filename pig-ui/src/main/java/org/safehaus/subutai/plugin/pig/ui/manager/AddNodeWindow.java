0408db588b8da0958280875a016df42d55cbaccf
dilshat <daliev@critical-factor.com>
2015-01-20 17:34:10 +0600

    updating references to moved classes

--- pig-ui/src/main/java/org/safehaus/subutai/plugin/pig/ui/manager/AddNodeWindow.java
+++ pig-ui/src/main/java/org/safehaus/subutai/plugin/pig/ui/manager/AddNodeWindow.java
@@ -5,9 +5,9 @@ import java.util.Set;
 import java.util.UUID;
 import java.util.concurrent.ExecutorService;
 
+import org.safehaus.subutai.common.peer.ContainerHost;
 import org.safehaus.subutai.common.tracker.OperationState;
 import org.safehaus.subutai.common.tracker.TrackerOperationView;
-import org.safehaus.subutai.core.peer.api.ContainerHost;
 import org.safehaus.subutai.core.tracker.api.Tracker;
 import org.safehaus.subutai.plugin.pig.api.Pig;
 import org.safehaus.subutai.plugin.pig.api.PigConfig;
