d009b47e71a454bd559e8789732691730385bee1
dilshat <daliev@critical-factor.com>
2015-01-20 17:39:35 +0600

    updating references to moved classes

--- hipi-ui/src/main/java/org/safehaus/subutai/plugin/hipi/ui/manager/AddNodeWindow.java
+++ hipi-ui/src/main/java/org/safehaus/subutai/plugin/hipi/ui/manager/AddNodeWindow.java
@@ -10,9 +10,9 @@ import java.util.Set;
 import java.util.UUID;
 import java.util.concurrent.ExecutorService;
 
+import org.safehaus.subutai.common.peer.ContainerHost;
 import org.safehaus.subutai.common.tracker.OperationState;
 import org.safehaus.subutai.common.tracker.TrackerOperationView;
-import org.safehaus.subutai.core.peer.api.ContainerHost;
 import org.safehaus.subutai.core.tracker.api.Tracker;
 import org.safehaus.subutai.plugin.hipi.api.Hipi;
 import org.safehaus.subutai.plugin.hipi.api.HipiConfig;
