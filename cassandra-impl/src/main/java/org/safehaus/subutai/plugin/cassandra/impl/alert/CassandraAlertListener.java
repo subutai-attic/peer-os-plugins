494b0d249fc165ce489bf90af123c02dcdf3e9d6
dilshat <daliev@critical-factor.com>
2015-02-06 15:27:56 +0600

    Fixed references to moved classes

--- cassandra-impl/src/main/java/org/safehaus/subutai/plugin/cassandra/impl/alert/CassandraAlertListener.java
+++ cassandra-impl/src/main/java/org/safehaus/subutai/plugin/cassandra/impl/alert/CassandraAlertListener.java
@@ -8,6 +8,7 @@ import java.util.regex.Pattern;
 
 import org.safehaus.subutai.common.command.CommandException;
 import org.safehaus.subutai.common.command.CommandResult;
+import org.safehaus.subutai.common.command.CommandUtil;
 import org.safehaus.subutai.common.command.RequestBuilder;
 import org.safehaus.subutai.common.environment.Environment;
 import org.safehaus.subutai.common.environment.EnvironmentNotFoundException;
@@ -16,7 +17,6 @@ import org.safehaus.subutai.common.peer.ContainerHost;
 import org.safehaus.subutai.core.metric.api.AlertListener;
 import org.safehaus.subutai.core.metric.api.ContainerHostMetric;
 import org.safehaus.subutai.core.metric.api.MonitoringSettings;
-import org.safehaus.subutai.core.peer.api.CommandUtil;
 import org.safehaus.subutai.plugin.cassandra.api.CassandraClusterConfig;
 import org.safehaus.subutai.plugin.cassandra.impl.CassandraImpl;
 import org.safehaus.subutai.plugin.cassandra.impl.Commands;
