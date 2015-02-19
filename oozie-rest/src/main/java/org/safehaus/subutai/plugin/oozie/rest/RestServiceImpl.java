7331b752f727322a3c2f9181186da99f3add5b82
Ebru Cetinkaya <ecetinkaya@safehaus.org>
2015-02-19 17:14:34 +0200

    minor fix

--- oozie-rest/src/main/java/org/safehaus/subutai/plugin/oozie/rest/RestServiceImpl.java
+++ oozie-rest/src/main/java/org/safehaus/subutai/plugin/oozie/rest/RestServiceImpl.java
@@ -225,7 +225,7 @@ public class RestServiceImpl implements RestService
             {
                 break;
             }
-            if ( System.currentTimeMillis() - start > ( 90 * 1000 ) )
+            if ( System.currentTimeMillis() - start > ( 200 * 1000 ) )
             {
                 break;
             }
