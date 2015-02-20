e2a10857e4cc57dcac955c9cc092adba11cfc41f
Ebru Cetinkaya <ecetinkaya@safehaus.org>
2015-02-20 17:35:39 +0200

    rests are fixed

---
+++ presto-rest/src/main/java/org/safehaus/subutai/plugin/presto/rest/RestServiceImpl.java
@@ -0,0 +1,273 @@
+package org.safehaus.subutai.plugin.presto.rest;
+
+
+import java.util.List;
+import java.util.Set;
+import java.util.UUID;
+
+import javax.ws.rs.core.Response;
+
+import org.safehaus.subutai.common.tracker.OperationState;
+import org.safehaus.subutai.common.tracker.TrackerOperationView;
+import org.safehaus.subutai.common.util.JsonUtil;
+import org.safehaus.subutai.core.tracker.api.Tracker;
+import org.safehaus.subutai.plugin.presto.api.Presto;
+import org.safehaus.subutai.plugin.presto.api.PrestoClusterConfig;
+
+import com.google.common.base.Preconditions;
+import com.google.common.collect.Sets;
+
+
+public class RestServiceImpl implements RestService
+{
+    private Presto prestoManager;
+    private Tracker tracker;
+
+
+    public RestServiceImpl( final Presto prestoManager )
+    {
+        this.prestoManager = prestoManager;
+    }
+
+
+    @Override
+    public Response listClusters()
+    {
+        List<PrestoClusterConfig> configs = prestoManager.getClusters();
+        Set<String> clusterNames = Sets.newHashSet();
+
+        for ( PrestoClusterConfig config : configs )
+        {
+            clusterNames.add( config.getClusterName() );
+        }
+
+        String clusters = JsonUtil.GSON.toJson( clusterNames );
+        return Response.status( Response.Status.OK ).entity( clusters ).build();
+    }
+
+
+    @Override
+    public Response getCluster( final String clusterName )
+    {
+        PrestoClusterConfig config = prestoManager.getCluster( clusterName );
+
+        String cluster = JsonUtil.GSON.toJson( config );
+        return Response.status( Response.Status.OK ).entity( cluster ).build();
+    }
+
+
+    @Override
+    public Response installCluster( final String clusterName, final String hadoopClusterName, final String masterNode,
+                                    final String workers )
+    {
+        Preconditions.checkNotNull( clusterName );
+        Preconditions.checkNotNull( hadoopClusterName );
+        Preconditions.checkNotNull( masterNode );
+        Preconditions.checkNotNull( workers );
+
+        PrestoClusterConfig config = new PrestoClusterConfig();
+        config.setClusterName( clusterName );
+        config.setHadoopClusterName( hadoopClusterName );
+        config.setCoordinatorNode( UUID.fromString( masterNode ) );
+
+        String[] arr = workers.replaceAll( "\\s+", "" ).split( "," );
+        for ( String node : arr )
+        {
+            config.getWorkers().add( UUID.fromString( node ) );
+        }
+
+        UUID uuid = prestoManager.installCluster( config );
+        waitUntilOperationFinish( uuid );
+        OperationState state = waitUntilOperationFinish( uuid );
+        return createResponse( uuid, state );
+    }
+
+
+    @Override
+    public Response uninstallCluster( final String clusterName )
+    {
+        Preconditions.checkNotNull( clusterName );
+        if ( prestoManager.getCluster( clusterName ) == null )
+        {
+            return Response.status( Response.Status.INTERNAL_SERVER_ERROR ).
+                    entity( clusterName + " cluster not found." ).build();
+        }
+        UUID uuid = prestoManager.uninstallCluster( clusterName );
+        OperationState state = waitUntilOperationFinish( uuid );
+        return createResponse( uuid, state );
+    }
+
+
+    @Override
+    public Response addWorkerNode( final String clusterName, final String lxcHostName )
+    {
+        Preconditions.checkNotNull( clusterName );
+        Preconditions.checkNotNull( lxcHostName );
+        if ( prestoManager.getCluster( clusterName ) == null )
+        {
+            return Response.status( Response.Status.INTERNAL_SERVER_ERROR ).
+                    entity( clusterName + " cluster not found." ).build();
+        }
+        UUID uuid = prestoManager.addWorkerNode( clusterName, lxcHostName );
+        OperationState state = waitUntilOperationFinish( uuid );
+        return createResponse( uuid, state );
+    }
+
+
+    @Override
+    public Response destroyWorkerNode( final String clusterName, final String lxcHostName )
+    {
+        Preconditions.checkNotNull( clusterName );
+        Preconditions.checkNotNull( lxcHostName );
+        if ( prestoManager.getCluster( clusterName ) == null )
+        {
+            return Response.status( Response.Status.INTERNAL_SERVER_ERROR ).
+                    entity( clusterName + " cluster not found." ).build();
+        }
+        UUID uuid = prestoManager.destroyWorkerNode( clusterName, lxcHostName );
+        OperationState state = waitUntilOperationFinish( uuid );
+        return createResponse( uuid, state );
+    }
+
+
+    @Override
+    public Response startNode( final String clusterName, final String lxcHostName )
+    {
+        Preconditions.checkNotNull( clusterName );
+        Preconditions.checkNotNull( lxcHostName );
+        if ( prestoManager.getCluster( clusterName ) == null )
+        {
+            return Response.status( Response.Status.INTERNAL_SERVER_ERROR ).
+                    entity( clusterName + " cluster not found." ).build();
+        }
+        UUID uuid = prestoManager.startNode( clusterName, lxcHostName );
+        OperationState state = waitUntilOperationFinish( uuid );
+        return createResponse( uuid, state );
+    }
+
+
+    @Override
+    public Response stopNode( final String clusterName, final String lxcHostName )
+    {
+        Preconditions.checkNotNull( clusterName );
+        Preconditions.checkNotNull( lxcHostName );
+        if ( prestoManager.getCluster( clusterName ) == null )
+        {
+            return Response.status( Response.Status.INTERNAL_SERVER_ERROR ).
+                    entity( clusterName + " cluster not found." ).build();
+        }
+        UUID uuid = prestoManager.stopNode( clusterName, lxcHostName );
+        OperationState state = waitUntilOperationFinish( uuid );
+        return createResponse( uuid, state );
+    }
+
+
+    @Override
+    public Response checkNode( final String clusterName, final String lxcHostName )
+    {
+        Preconditions.checkNotNull( clusterName );
+        Preconditions.checkNotNull( lxcHostName );
+        if ( prestoManager.getCluster( clusterName ) == null )
+        {
+            return Response.status( Response.Status.INTERNAL_SERVER_ERROR ).
+                    entity( clusterName + " cluster not found." ).build();
+        }
+        UUID uuid = prestoManager.checkNode( clusterName, lxcHostName );
+        OperationState state = waitUntilOperationFinish( uuid );
+        return createResponse( uuid, state );
+    }
+
+
+    @Override
+    public Response startCluster( final String clusterName )
+    {
+        Preconditions.checkNotNull( clusterName );
+        if ( prestoManager.getCluster( clusterName ) == null )
+        {
+            return Response.status( Response.Status.INTERNAL_SERVER_ERROR ).
+                    entity( clusterName + " cluster not found." ).build();
+        }
+        UUID uuid = prestoManager.stopAllNodes( clusterName );
+        waitUntilOperationFinish( uuid );
+        OperationState state = waitUntilOperationFinish( uuid );
+        return createResponse( uuid, state );
+    }
+
+
+    @Override
+    public Response stopCluster( final String clusterName )
+    {
+        Preconditions.checkNotNull( clusterName );
+        if ( prestoManager.getCluster( clusterName ) == null )
+        {
+            return Response.status( Response.Status.INTERNAL_SERVER_ERROR ).
+                    entity( clusterName + " cluster not found." ).build();
+        }
+        UUID uuid = prestoManager.stopAllNodes( clusterName );
+        waitUntilOperationFinish( uuid );
+        OperationState state = waitUntilOperationFinish( uuid );
+        return createResponse( uuid, state );
+    }
+
+
+    private Response createResponse( UUID uuid, OperationState state )
+    {
+        TrackerOperationView po = tracker.getTrackerOperation( PrestoClusterConfig.PRODUCT_KEY, uuid );
+        if ( state == OperationState.FAILED )
+        {
+            return Response.status( Response.Status.INTERNAL_SERVER_ERROR ).entity( po.getLog() ).build();
+        }
+        else if ( state == OperationState.SUCCEEDED )
+        {
+            return Response.status( Response.Status.OK ).entity( po.getLog() ).build();
+        }
+        else
+        {
+            return Response.status( Response.Status.INTERNAL_SERVER_ERROR ).entity( "Timeout" ).build();
+        }
+    }
+
+
+    private OperationState waitUntilOperationFinish( UUID uuid )
+    {
+        OperationState state = null;
+        long start = System.currentTimeMillis();
+        while ( !Thread.interrupted() )
+        {
+            TrackerOperationView po = tracker.getTrackerOperation( PrestoClusterConfig.PRODUCT_KEY, uuid );
+            if ( po != null )
+            {
+                if ( po.getState() != OperationState.RUNNING )
+                {
+                    state = po.getState();
+                    break;
+                }
+            }
+            try
+            {
+                Thread.sleep( 1000 );
+            }
+            catch ( InterruptedException ex )
+            {
+                break;
+            }
+            if ( System.currentTimeMillis() - start > ( 200 * 1000 ) )
+            {
+                break;
+            }
+        }
+        return state;
+    }
+
+
+    public Tracker getTracker()
+    {
+        return tracker;
+    }
+
+
+    public void setTracker( final Tracker tracker )
+    {
+        this.tracker = tracker;
+    }
+}
