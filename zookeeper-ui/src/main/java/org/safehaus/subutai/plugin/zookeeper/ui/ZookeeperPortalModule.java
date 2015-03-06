0de7084980c6841cb0a24c23be79c5eb7ba39729
Ebru Cetinkaya <ecetinkaya@safehaus.org>
2015-03-06 14:32:55 +0200

    executor fix

--- zookeeper-ui/src/main/java/org/safehaus/subutai/plugin/zookeeper/ui/ZookeeperPortalModule.java
+++ zookeeper-ui/src/main/java/org/safehaus/subutai/plugin/zookeeper/ui/ZookeeperPortalModule.java
@@ -7,6 +7,7 @@ import java.util.concurrent.Executors;
 
 import javax.naming.NamingException;
 
+import org.safehaus.subutai.common.mdc.SubutaiExecutors;
 import org.safehaus.subutai.common.util.FileUtil;
 import org.safehaus.subutai.core.env.api.EnvironmentManager;
 import org.safehaus.subutai.core.tracker.api.Tracker;
@@ -43,7 +44,7 @@ public class ZookeeperPortalModule implements PortalModule
 
     public void init()
     {
-        executor = Executors.newCachedThreadPool();
+        executor = SubutaiExecutors.newCachedThreadPool();
     }
 
 
