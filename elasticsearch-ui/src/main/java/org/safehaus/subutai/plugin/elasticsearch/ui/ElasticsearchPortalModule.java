60fc8d86b43353ce9c56ca671a9cf5afbca1017f
Salih Kardan <salihkardan@gmail.com>
2015-03-06 14:16:37 +0200

    change executor initialization

--- elasticsearch-ui/src/main/java/org/safehaus/subutai/plugin/elasticsearch/ui/ElasticsearchPortalModule.java
+++ elasticsearch-ui/src/main/java/org/safehaus/subutai/plugin/elasticsearch/ui/ElasticsearchPortalModule.java
@@ -8,6 +8,7 @@ import java.util.logging.Logger;
 
 import javax.naming.NamingException;
 
+import org.safehaus.subutai.common.mdc.SubutaiExecutors;
 import org.safehaus.subutai.common.util.FileUtil;
 import org.safehaus.subutai.core.env.api.EnvironmentManager;
 import org.safehaus.subutai.core.tracker.api.Tracker;
@@ -41,7 +42,7 @@ public class ElasticsearchPortalModule implements PortalModule
 
     public void init()
     {
-        executor = Executors.newCachedThreadPool();
+        executor = SubutaiExecutors.newCachedThreadPool();
     }
 
 
