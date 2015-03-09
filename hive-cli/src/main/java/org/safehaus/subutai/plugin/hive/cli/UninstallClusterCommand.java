9ada8829dc889570d39eb788deca8c1fa0b13d3e
Eric <ermek.abyshev@gmail.com>
2015-03-09 19:08:44 +0600

    cli modified

--- hive-cli/src/main/java/org/safehaus/subutai/plugin/hive/cli/UninstallClusterCommand.java
+++ hive-cli/src/main/java/org/safehaus/subutai/plugin/hive/cli/UninstallClusterCommand.java
@@ -3,11 +3,8 @@ package org.safehaus.subutai.plugin.hive.cli;
 
 import java.util.UUID;
 
-import org.safehaus.subutai.common.tracker.OperationState;
-import org.safehaus.subutai.common.tracker.TrackerOperationView;
 import org.safehaus.subutai.core.tracker.api.Tracker;
 import org.safehaus.subutai.plugin.hive.api.Hive;
-import org.safehaus.subutai.plugin.hive.api.HiveConfig;
 
 import org.apache.karaf.shell.commands.Argument;
 import org.apache.karaf.shell.commands.Command;
@@ -17,15 +14,23 @@ import org.apache.karaf.shell.console.OsgiCommandSupport;
 /**
  * Displays the last log entries
  */
-@Command(scope = "hive", name = "uninstall-cluster", description = "Command to uninstall Hive cluster")
+@Command( scope = "hive", name = "uninstall-cluster", description = "Command to uninstall Hive cluster" )
 public class UninstallClusterCommand extends OsgiCommandSupport
 {
 
-    @Argument(index = 0, name = "clusterName", description = "The name of the cluster.", required = true,
-            multiValued = false)
-    String clusterName = null;
+    @Argument( index = 0, name = "clusterName", required = true, multiValued = false, description = "Delete cluster" )
+    String clusterName;
     private Hive hiveManager;
     private Tracker tracker;
+
+
+    protected Object doExecute()
+    {
+        UUID uuid = hiveManager.uninstallCluster( clusterName );
+        System.out.println(
+                "Uninstall operation is " + StartClusterCommand.waitUntilOperationFinish( tracker, uuid ) + "." );
+        return null;
+    }
 
 
     public Tracker getTracker()
@@ -49,43 +54,5 @@ public class UninstallClusterCommand extends OsgiCommandSupport
     public void setHiveManager( Hive hiveManager )
     {
         this.hiveManager = hiveManager;
-    }
-
-
-    protected Object doExecute()
-    {
-        UUID uuid = hiveManager.uninstallCluster( clusterName );
-        int logSize = 0;
-        while ( !Thread.interrupted() )
-        {
-            TrackerOperationView po = tracker.getTrackerOperation( HiveConfig.PRODUCT_KEY, uuid );
-            if ( po != null )
-            {
-                if ( logSize != po.getLog().length() )
-                {
-                    System.out.print( po.getLog().substring( logSize, po.getLog().length() ) );
-                    System.out.flush();
-                    logSize = po.getLog().length();
-                }
-                if ( po.getState() != OperationState.RUNNING )
-                {
-                    break;
-                }
-            }
-            else
-            {
-                System.out.println( "Product operation not found. Check logs" );
-                break;
-            }
-            try
-            {
-                Thread.sleep( 1000 );
-            }
-            catch ( InterruptedException ex )
-            {
-                break;
-            }
-        }
-        return null;
     }
 }
