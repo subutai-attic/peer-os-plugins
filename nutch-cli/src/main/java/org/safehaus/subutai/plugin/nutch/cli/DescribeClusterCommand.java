1c6fa2dddd5a6747d27ed02803c1d595e2ad6377
Eric <ermek.abyshev@gmail.com>
2015-03-10 14:37:57 +0600

    cli modified

---
+++ nutch-cli/src/main/java/org/safehaus/subutai/plugin/nutch/cli/DescribeClusterCommand.java
@@ -0,0 +1,91 @@
+package org.safehaus.subutai.plugin.nutch.cli;
+
+
+import java.util.UUID;
+
+import org.safehaus.subutai.common.environment.ContainerHostNotFoundException;
+import org.safehaus.subutai.common.environment.Environment;
+import org.safehaus.subutai.common.environment.EnvironmentNotFoundException;
+import org.safehaus.subutai.core.env.api.EnvironmentManager;
+import org.safehaus.subutai.plugin.nutch.api.Nutch;
+import org.safehaus.subutai.plugin.nutch.api.NutchConfig;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.apache.karaf.shell.commands.Argument;
+import org.apache.karaf.shell.commands.Command;
+import org.apache.karaf.shell.console.OsgiCommandSupport;
+
+
+/**
+ * sample command : nutch:describe-cluster test \ {cluster name}
+ */
+@Command( scope = "nutch", name = "describe-cluster", description = "Shows the details of the Nutch cluster." )
+public class DescribeClusterCommand extends OsgiCommandSupport
+{
+
+    @Argument( index = 0, name = "clusterName", description = "The name of the cluster.", required = true,
+            multiValued = false )
+    String clusterName = null;
+    private Nutch nutchManager;
+    private EnvironmentManager environmentManager;
+    private static final Logger LOG = LoggerFactory.getLogger( DescribeClusterCommand.class.getName() );
+
+
+    protected Object doExecute()
+    {
+        NutchConfig config = nutchManager.getCluster( clusterName );
+        try
+        {
+            Environment environment = environmentManager.findEnvironment( config.getEnvironmentId() );
+            StringBuilder sb = new StringBuilder();
+            sb.append( "Cluster name: " ).append( config.getClusterName() ).append( "\n" );
+            sb.append( "Nodes:" ).append( "\n" );
+            for ( UUID containerId : config.getNodes() )
+            {
+                try
+                {
+                    sb.append( "   Container Hostname: " ).
+                            append( environment.getContainerHostById( containerId ).getHostname() ).append( "\n" );
+                }
+                catch ( ContainerHostNotFoundException e )
+                {
+                    LOG.error( "Could not find container host !!!" );
+                    e.printStackTrace();
+                }
+            }
+            System.out.println( sb.toString() );
+        }
+        catch ( EnvironmentNotFoundException e )
+        {
+            LOG.error( "Could not find environment !!! " );
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+
+    public Nutch getNutchManager()
+    {
+        return nutchManager;
+    }
+
+
+    public void setNutchManager( final Nutch nutchManager )
+    {
+        this.nutchManager = nutchManager;
+    }
+
+
+    public EnvironmentManager getEnvironmentManager()
+    {
+        return environmentManager;
+    }
+
+
+    public void setEnvironmentManager( final EnvironmentManager environmentManager )
+    {
+        this.environmentManager = environmentManager;
+    }
+}
