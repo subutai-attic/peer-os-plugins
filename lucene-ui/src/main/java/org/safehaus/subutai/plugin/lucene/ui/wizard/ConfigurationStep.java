cb8790ea46d4d42d0a56eb3fe9117c94410407e2
Eric <ermek.abyshev@gmail.com>
2015-04-02 17:58:00 +0600

    added filter for installed nodes and added progress icon in manage tab

--- lucene-ui/src/main/java/org/safehaus/subutai/plugin/lucene/ui/wizard/ConfigurationStep.java
+++ lucene-ui/src/main/java/org/safehaus/subutai/plugin/lucene/ui/wizard/ConfigurationStep.java
@@ -8,6 +8,7 @@ import java.util.Set;
 import java.util.UUID;
 
 import org.safehaus.subutai.common.environment.ContainerHostNotFoundException;
+import org.safehaus.subutai.common.environment.Environment;
 import org.safehaus.subutai.common.environment.EnvironmentNotFoundException;
 import org.safehaus.subutai.common.peer.ContainerHost;
 import org.safehaus.subutai.common.util.CollectionUtil;
@@ -15,6 +16,8 @@ import org.safehaus.subutai.core.env.api.EnvironmentManager;
 import org.safehaus.subutai.plugin.hadoop.api.Hadoop;
 import org.safehaus.subutai.plugin.hadoop.api.HadoopClusterConfig;
 import org.safehaus.subutai.plugin.lucene.api.LuceneConfig;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.google.common.base.Strings;
 import com.google.common.collect.Sets;
@@ -35,10 +38,12 @@ import com.vaadin.ui.VerticalLayout;
 
 public class ConfigurationStep extends Panel
 {
+    private final static Logger LOGGER = LoggerFactory.getLogger( ConfigurationStep.class );
+
     private final Hadoop hadoop;
     private final EnvironmentManager environmentManager;
     final Wizard wizard;
-
+    private Environment hadoopEnvironment;
 
     public ConfigurationStep( final Hadoop hadoop, final Wizard wizard, final EnvironmentManager environmentManager )
     {
@@ -103,6 +108,7 @@ public class ConfigurationStep extends Panel
     {
         TextField nameTxt = new TextField( "Cluster name" );
         nameTxt.setId( "luceneClusterName" );
+        nameTxt.setInputPrompt( "Cluster Name" );
         nameTxt.setRequired( true );
         nameTxt.addValueChangeListener( new Property.ValueChangeListener()
         {
@@ -132,21 +138,29 @@ public class ConfigurationStep extends Panel
                 {
                     HadoopClusterConfig hadoopInfo = ( HadoopClusterConfig ) event.getProperty().getValue();
                     config.setHadoopClusterName( hadoopInfo.getClusterName() );
-                    Set<ContainerHost> hadoopNodes;
+                    config.getNodes().clear();
                     try
                     {
-                        hadoopNodes = environmentManager.findEnvironment( hadoopInfo.getEnvironmentId() )
-                                                        .getContainerHostsByIds(
-                                                                Sets.newHashSet( hadoopInfo.getAllNodes() ) );
+                        hadoopEnvironment = environmentManager.findEnvironment( hadoopInfo.getEnvironmentId() );
+                    }
+                    catch ( EnvironmentNotFoundException e )
+                    {
+                        LOGGER.error( "Error getting environment by id: " + hadoopInfo.getEnvironmentId().toString(),
+                                e );
+                        return;
+                    }
+
+                    Set<ContainerHost> hadoopNodes = Sets.newHashSet();
+                    try
+                    {
+                        for ( UUID nodeId : filterNodes( hadoopInfo.getAllNodes() ) )
+                        {
+                            hadoopNodes.add( hadoopEnvironment.getContainerHostById( nodeId ) );
+                        }
                     }
                     catch ( ContainerHostNotFoundException e )
                     {
                         show( String.format( "Failed obtaining environment containers: %s", e ) );
-                        return;
-                    }
-                    catch ( EnvironmentNotFoundException e )
-                    {
-                        show( String.format( "Environment not found: %s", e ) );
                         return;
                     }
                     select.setValue( null );
@@ -214,6 +228,26 @@ public class ConfigurationStep extends Panel
         parent.addComponent( nameTxt );
         parent.addComponent( hadoopClusters );
         parent.addComponent( select );
+    }
+
+
+    //exclude hadoop nodes that are already in another lucene cluster
+    private List<UUID> filterNodes( List<UUID> hadoopNodes )
+    {
+        List<UUID> luceneNodes = new ArrayList<>();
+        List<UUID> filteredNodes = new ArrayList<>();
+        for ( LuceneConfig luceneConfig : wizard.getLuceneManager().getClusters() )
+        {
+            luceneNodes.addAll( luceneConfig.getNodes() );
+        }
+        for ( UUID node : hadoopNodes )
+        {
+            if ( !luceneNodes.contains( node ) )
+            {
+                filteredNodes.add( node );
+            }
+        }
+        return filteredNodes;
     }
 
 
