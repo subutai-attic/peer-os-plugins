4c4c82cd86dc90673a939ebd2a48deb01e2539ad
Ebru Cetinkaya <ecetinkaya@safehaus.org>
2015-04-02 17:57:40 +0300

    auto-scale rest is added

--- presto-rest/src/main/java/org/safehaus/subutai/plugin/presto/rest/RestService.java
+++ presto-rest/src/main/java/org/safehaus/subutai/plugin/presto/rest/RestService.java
@@ -105,4 +105,11 @@ public interface RestService
     @Path( "clusters/{clusterName}/check" )
     @Produces( { MediaType.APPLICATION_JSON } )
     public Response checkCluster( @PathParam( "clusterName" ) String clusterName );
+
+    //auto-scale cluster
+    @POST
+    @Path("clusters/{clusterName}/auto_scale/{scale}")
+    @Produces({ MediaType.APPLICATION_JSON })
+    public Response autoScaleCluster( @PathParam("clusterName") String clusterName,
+                                      @PathParam( "scale" ) boolean scale );
 }
