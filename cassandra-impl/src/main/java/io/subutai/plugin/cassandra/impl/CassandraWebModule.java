ce8390895868085f13d0c0688b49dcfb7c76b071
istus <istus@mail.critical-factor.com>
2015-11-20 22:36:39 +0600

    SS-3858, added, cassandra icon

---
+++ cassandra-impl/src/main/java/io/subutai/plugin/cassandra/impl/CassandraWebModule.java
@@ -0,0 +1,41 @@
+package io.subutai.plugin.cassandra.impl;
+
+
+import io.subutai.webui.api.WebuiModule;
+
+
+public class CassandraWebModule implements WebuiModule
+{
+    public static String NAME = "Cassandra";
+    public static String IMG = "plugins/cassandra/cassandra.png";
+
+    public CassandraWebModule()
+    {
+
+    }
+
+    @Override
+    public String getName()
+    {
+        return NAME;
+    }
+
+
+    @Override
+    public String getModuleInfo()
+    {
+        return String.format( "{\"img\" : \"%s\", \"name\" : \"%s\"}", IMG, NAME );
+    }
+
+
+    @Override
+    public String getAngularDependecyList()
+    {
+        return String.format( "{" +
+                "name: 'subutai.blueprints', files: ["
+                + "'subutai-app/blueprints/blueprints.js',"
+                + "'subutai-app/blueprints/controller.js',"
+                + "'subutai-app/environment/service.js'"
+                + "]}" );
+    }
+}
