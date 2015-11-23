0c48936a23486bc707a5439f3e44ee7ceca4e403
Timur Jamakeev <tjamakeev@critical-factor.com>
2015-11-23 18:36:22 +0600

    fixed cassandra plugin, SS-3083

--- cassandra-impl/src/main/java/io/subutai/plugin/cassandra/impl/alert/CassandraAlertListener.java
+++ cassandra-impl/src/main/java/io/subutai/plugin/cassandra/impl/alert/CassandraAlertListener.java
@@ -18,6 +18,8 @@ import io.subutai.common.environment.EnvironmentNotFoundException;
 import io.subutai.common.metric.ContainerHostMetric;
 import io.subutai.common.metric.ProcessResourceUsage;
 import io.subutai.common.peer.EnvironmentContainerHost;
+import io.subutai.common.quota.RamQuota;
+import io.subutai.common.quota.RamQuotaUnit;
 import io.subutai.core.metric.api.AlertListener;
 import io.subutai.core.metric.api.MonitoringSettings;
 import io.subutai.plugin.cassandra.api.CassandraClusterConfig;
@@ -115,7 +117,7 @@ public class CassandraAlertListener implements AlertListener
 
         // Set 50 percent of the available ram capacity of the resource host
         // to maximum ram quota limit assignable to the container
-        MAX_RAM_QUOTA_MB = sourceHost.getAvailableRamQuota() * 0.5;
+        MAX_RAM_QUOTA_MB = sourceHost.getAvailableRamQuota().getRamQuotaValue( RamQuotaUnit.MB ) * 0.5;
 
 
         //figure out process pid
@@ -166,21 +168,22 @@ public class CassandraAlertListener implements AlertListener
             if ( isRamStressed )
             {
                 //read current RAM quota
-                int ramQuota = sourceHost.getRamQuota();
+                double ramQuota = sourceHost.getRamQuota().getRamQuotaValue( RamQuotaUnit.MB );
 
                 if ( ramQuota < MAX_RAM_QUOTA_MB )
                 {
 
                     // if available quota on resource host is greater than 10 % of calculated increase amount,
                     // increase quota, otherwise scale horizontally
-                    int newRamQuota = ramQuota * ( 100 + RAM_QUOTA_INCREMENT_PERCENTAGE ) / 100;
+                    double newRamQuota = ramQuota * ( 100 + RAM_QUOTA_INCREMENT_PERCENTAGE ) / 100;
                     if ( MAX_RAM_QUOTA_MB > newRamQuota )
                     {
 
                         LOG.info( "Increasing ram quota of {} from {} MB to {} MB.", sourceHost.getHostname(),
                                 sourceHost.getRamQuota(), newRamQuota );
                         //we can increase RAM quota
-                        sourceHost.setRamQuota( newRamQuota );
+                        RamQuota quota = new RamQuota( RamQuotaUnit.MB, ( long ) newRamQuota );
+                        sourceHost.setRamQuota( quota );
 
                         quotaIncreased = true;
                     }
@@ -191,7 +194,7 @@ public class CassandraAlertListener implements AlertListener
             {
 
                 //read current CPU quota
-                int cpuQuota = sourceHost.getCpuQuota();
+                int cpuQuota = sourceHost.getCpuQuota().getPercentage();
                 if ( cpuQuota < MAX_CPU_QUOTA_PERCENT )
                 {
                     int newCpuQuota = Math.min( MAX_CPU_QUOTA_PERCENT, cpuQuota + CPU_QUOTA_INCREMENT_PERCENT );
