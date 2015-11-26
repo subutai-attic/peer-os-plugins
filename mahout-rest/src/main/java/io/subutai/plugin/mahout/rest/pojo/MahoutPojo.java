56cc459b1be6ce4063baef72dd0c7166215f483c
Ermek Abyshev <eabyshev@critical-factor.com>
2015-11-26 19:50:54 +0600

    SS-3083 - added and updated RESTs

---
+++ mahout-rest/src/main/java/io/subutai/plugin/mahout/rest/pojo/MahoutPojo.java
@@ -0,0 +1,64 @@
+package io.subutai.plugin.mahout.rest.pojo;
+
+
+import java.util.Set;
+
+
+/**
+ * Created by ermek on 11/26/15.
+ */
+public class MahoutPojo
+{
+    private String clusterName;
+    private String hadoopClusterName;
+    private String environmentId;
+    private Set<ContainerPojo> nodes;
+
+
+    public String getClusterName()
+    {
+        return clusterName;
+    }
+
+
+    public void setClusterName( final String clusterName )
+    {
+        this.clusterName = clusterName;
+    }
+
+
+    public String getHadoopClusterName()
+    {
+        return hadoopClusterName;
+    }
+
+
+    public void setHadoopClusterName( final String hadoopClusterName )
+    {
+        this.hadoopClusterName = hadoopClusterName;
+    }
+
+
+    public String getEnvironmentId()
+    {
+        return environmentId;
+    }
+
+
+    public void setEnvironmentId( final String environmentId )
+    {
+        this.environmentId = environmentId;
+    }
+
+
+    public Set<ContainerPojo> getNodes()
+    {
+        return nodes;
+    }
+
+
+    public void setNodes( final Set<ContainerPojo> nodes )
+    {
+        this.nodes = nodes;
+    }
+}
