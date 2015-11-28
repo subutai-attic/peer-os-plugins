d716045a370eb5b408d6b89553916c2c08ef18e2
Ermek Abyshev <eabyshev@critical-factor.com>
2015-11-28 18:35:32 +0600

    SS-3083 - added and updated RESTs

--- presto-rest/src/main/java/io/subutai/plugin/presto/rest/RestService.java
+++ presto-rest/src/main/java/io/subutai/plugin/presto/rest/RestService.java
@@ -2,13 +2,13 @@ package io.subutai.plugin.presto.rest;
 
 
 import javax.ws.rs.DELETE;
+import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
@@ -34,10 +34,9 @@ public interface RestService
     @POST
     @Path( "clusters/install" )
     @Produces( { MediaType.APPLICATION_JSON } )
-    public Response installCluster( @QueryParam( "clusterName" ) String clusterName,
-                                    @QueryParam( "hadoopClusterName" ) String hadoopClusterName,
-                                    @QueryParam( "master" ) String master,
-                                    @QueryParam( "workers" ) String workers );
+    public Response installCluster( @FormParam( "clusterName" ) String clusterName,
+                                    @FormParam( "hadoopClusterName" ) String hadoopClusterName,
+                                    @FormParam( "master" ) String master, @FormParam( "workers" ) String workers );
 
 
     //destroy cluster
@@ -108,8 +107,14 @@ public interface RestService
 
     //auto-scale cluster
     @POST
-    @Path("clusters/{clusterName}/auto_scale/{scale}")
-    @Produces({ MediaType.APPLICATION_JSON })
-    public Response autoScaleCluster( @PathParam("clusterName") String clusterName,
+    @Path( "clusters/{clusterName}/auto_scale/{scale}" )
+    @Produces( { MediaType.APPLICATION_JSON } )
+    public Response autoScaleCluster( @PathParam( "clusterName" ) String clusterName,
                                       @PathParam( "scale" ) boolean scale );
+
+    //get available nodes for adding
+    @GET
+    @Path( "clusters/{clusterName}/available/nodes" )
+    @Produces( { MediaType.APPLICATION_JSON } )
+    public Response getAvailableNodes( @PathParam( "clusterName" ) String clusterName );
 }
