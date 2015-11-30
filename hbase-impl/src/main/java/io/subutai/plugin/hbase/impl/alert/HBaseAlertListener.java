6205e973e605bf3a19a7a5bcda66d8ff541aff30
Ermek Abyshev <eabyshev@critical-factor.com>
2015-11-30 17:43:45 +0600

    SS-3083 - added and updated rests

--- hbase-impl/src/main/java/io/subutai/plugin/hbase/impl/alert/HBaseAlertListener.java
+++ hbase-impl/src/main/java/io/subutai/plugin/hbase/impl/alert/HBaseAlertListener.java
@@ -195,43 +195,43 @@ public class HBaseAlertListener implements AlertListener
             if ( isRAMStressedByHBase )
             {
                 //read current RAM quota
-                int ramQuota = sourceHost.getRamQuota();
-
-                if ( ramQuota < MAX_RAM_QUOTA_MB )
-                {
-                    // if available quota on resource host is greater than 10 % of calculated increase amount,
-                    // increase quota, otherwise scale horizontally
-                    int newRamQuota = ramQuota * ( 100 + RAM_QUOTA_INCREMENT_PERCENTAGE ) / 100;
-                    if ( MAX_RAM_QUOTA_MB > newRamQuota )
-                    {
-
-                        LOG.info( "Increasing ram quota of {} from {} MB to {} MB.", sourceHost.getHostname(),
-                                sourceHost.getRamQuota(), newRamQuota );
-                        //we can increase RAM quota
-                        sourceHost.setRamQuota( newRamQuota );
-
-                        quotaIncreased = true;
-                    }
-                }
+//                int ramQuota = sourceHost.getRamQuota();
+//
+//                if ( ramQuota < MAX_RAM_QUOTA_MB )
+//                {
+//                    // if available quota on resource host is greater than 10 % of calculated increase amount,
+//                    // increase quota, otherwise scale horizontally
+//                    int newRamQuota = ramQuota * ( 100 + RAM_QUOTA_INCREMENT_PERCENTAGE ) / 100;
+//                    if ( MAX_RAM_QUOTA_MB > newRamQuota )
+//                    {
+//
+//                        LOG.info( "Increasing ram quota of {} from {} MB to {} MB.", sourceHost.getHostname(),
+//                                sourceHost.getRamQuota(), newRamQuota );
+//                        //we can increase RAM quota
+//                        sourceHost.setRamQuota( newRamQuota );
+//
+//                        quotaIncreased = true;
+//                    }
+//                }
             }
 
             if ( isCPUStressedByHBase )
             {
 
                 //read current CPU quota
-                int cpuQuota = sourceHost.getCpuQuota();
-
-                if ( cpuQuota < MAX_CPU_QUOTA_PERCENT )
-                {
-                    int newCpuQuota = Math.min( MAX_CPU_QUOTA_PERCENT, cpuQuota + CPU_QUOTA_INCREMENT_PERCENT );
-                    LOG.info( "Increasing cpu quota of {} from {}% to {}%.", sourceHost.getHostname(), cpuQuota,
-                            newCpuQuota );
-
-                    //we can increase CPU quota
-                    sourceHost.setCpuQuota( newCpuQuota );
-
-                    quotaIncreased = true;
-                }
+//                int cpuQuota = sourceHost.getCpuQuota();
+//
+//                if ( cpuQuota < MAX_CPU_QUOTA_PERCENT )
+//                {
+//                    int newCpuQuota = Math.min( MAX_CPU_QUOTA_PERCENT, cpuQuota + CPU_QUOTA_INCREMENT_PERCENT );
+//                    LOG.info( "Increasing cpu quota of {} from {}% to {}%.", sourceHost.getHostname(), cpuQuota,
+//                            newCpuQuota );
+//
+//                    //we can increase CPU quota
+//                    sourceHost.setCpuQuota( newCpuQuota );
+//
+//                    quotaIncreased = true;
+//                }
             }
 
             //quota increase is made, return
--- hbase-rest/src/main/java/io/subutai/plugin/hbase/rest/RestService.java
+++ hbase-rest/src/main/java/io/subutai/plugin/hbase/rest/RestService.java
@@ -2,13 +2,13 @@ package io.subutai.plugin.hbase.rest;
 
 
 import javax.ws.rs.DELETE;
+import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
@@ -34,7 +34,7 @@ public interface RestService
     @POST
     @Path( "clusters" )
     @Produces( { MediaType.APPLICATION_JSON } )
-    public Response configureCluster( @QueryParam( "config" ) String config );
+    public Response configureCluster( @FormParam( "config" ) String config );
 
 
     //destroy cluster
