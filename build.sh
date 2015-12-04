00a195528906751e676c13fb8a00a30d70f5ffba
daralbaev <daralbaev@gmail.com>
2015-12-04 19:59:48 +0600

    SS-3083
    * changed version to 4.0.0-RC5

--- build.sh
+++ build.sh
@@ -1,1 +1,2 @@
-mvn clean install -Dmaven.test.skip=true && scp generic-api/target/generic-plugin-api-4.0.0-RC4.jar generic-cli/target/generic-plugin-cli-4.0.0-RC4.jar generic-impl/target/generic-plugin-impl-4.0.0-RC4.jar generic-rest/target/generic-plugin-rest-4.0.0-RC4.jar root@management4.critical-factor.com:/root/ermek/subutai-4.0.0-RC4/deploy
+#!/usr/bin/env bash
+mvn clean install -Dmaven.test.skip=true && scp generic-api/target/generic-plugin-api-4.0.0-RC5.jar generic-cli/target/generic-plugin-cli-4.0.0-RC5.jar generic-impl/target/generic-plugin-impl-4.0.0-RC5.jar generic-rest/target/generic-plugin-rest-4.0.0-RC5.jar root@management4.critical-factor.com:/root/ermek/subutai-4.0.0-RC5/deploy
