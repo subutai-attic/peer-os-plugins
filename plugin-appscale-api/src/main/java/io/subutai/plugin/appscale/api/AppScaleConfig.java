19d349829d0ffe995529942dc84d1b0ccc0523f4
Beyazit Kelceoglu <bkelceoglu@critical-factor.com>
2016-01-02 18:08:58 +0200

    APPSTALK-17
    modified some angular files.

--- plugin-appscale-api/src/main/java/io/subutai/plugin/appscale/api/AppScaleConfig.java
+++ plugin-appscale-api/src/main/java/io/subutai/plugin/appscale/api/AppScaleConfig.java
@@ -16,7 +16,7 @@ public class AppScaleConfig implements ConfigBase
 {
     public static final String PRODUCT_NAME = "Appscale";
     public static final String PRODUCT_KEY = "Appscale";
-    public static final String PACKAGE_NAME = ( Common.PACKAGE_PREFIX + PRODUCT_NAME ).toLowerCase();
+    public static final String PACKAGE_NAME = ( Common.PACKAGE_PREFIX + PRODUCT_NAME ).toLowerCase ();
 
     private static final String TEMPLATE_NAME = "Appscale";
     private String clusterName = "";
@@ -27,112 +27,112 @@ public class AppScaleConfig implements ConfigBase
     private String tracker;
 
 
-    public AppScaleConfig()
+    public AppScaleConfig ()
     {
-        nodes = new ArrayList<>();
+        nodes = new ArrayList<> ();
     }
 
 
     @Override
-    public String getClusterName()
+    public String getClusterName ()
     {
         return this.clusterName;
     }
 
 
-    public void setClusterName( String clusterName )
+    public void setClusterName ( String clusterName )
     {
         this.clusterName = clusterName;
     }
 
 
     @Override
-    public String getProductName()
+    public String getProductName ()
     {
         return AppScaleConfig.PRODUCT_NAME;
     }
 
 
     @Override
-    public String getProductKey()
+    public String getProductKey ()
     {
         return AppScaleConfig.PRODUCT_KEY;
     }
 
 
-    public List<String> getNodes()
+    public List<String> getNodes ()
     {
         return nodes;
     }
 
 
-    public void setNodes( List<String> nodes )
+    public void setNodes ( List<String> nodes )
     {
         this.nodes = nodes;
     }
 
 
-    public String getEnvironmentId()
+    public String getEnvironmentId ()
     {
         return environmentId;
     }
 
 
-    public void setEnvironmentId( String environmentId )
+    public void setEnvironmentId ( String environmentId )
     {
         this.environmentId = environmentId;
     }
 
 
-    public String getContainerType()
+    public String getContainerType ()
     {
         return containerType;
     }
 
 
-    public void setContainerType( String containerType )
+    public void setContainerType ( String containerType )
     {
         this.containerType = containerType;
     }
 
 
-    public String getTracker()
+    public String getTracker ()
     {
         return tracker;
     }
 
 
-    public void setTracker( String tracker )
+    public void setTracker ( String tracker )
     {
         this.tracker = tracker;
     }
 
 
-    public static String getPRODUCT_NAME()
+    public static String getPRODUCT_NAME ()
     {
         return PRODUCT_NAME;
     }
 
 
-    public static String getPRODUCT_KEY()
+    public static String getPRODUCT_KEY ()
     {
         return PRODUCT_KEY;
     }
 
 
-    public static String getPACKAGE_NAME()
+    public static String getPACKAGE_NAME ()
     {
         return PACKAGE_NAME;
     }
 
 
-    public static String getTEMPLATE_NAME()
+    public static String getTEMPLATE_NAME ()
     {
         return TEMPLATE_NAME;
     }
 
 
-    public String getDomainName()
+    public String getDomainName ()
     {
         return domainName;
     }
--- plugin-appscale-api/src/main/java/io/subutai/plugin/appscale/api/AppScaleInterface.java
+++ plugin-appscale-api/src/main/java/io/subutai/plugin/appscale/api/AppScaleInterface.java
@@ -1,6 +1,7 @@
 package io.subutai.plugin.appscale.api;
 
 
+import java.util.List;
 import java.util.UUID;
 
 import io.subutai.common.environment.Environment;
@@ -11,54 +12,61 @@ import io.subutai.plugin.common.api.ClusterSetupStrategy;
 
 public interface AppScaleInterface extends ApiBase<AppScaleConfig>
 {
+    @Override
+    public List<AppScaleConfig> getClusters ();
+
 
     @Override
-    UUID installCluster( AppScaleConfig appScaleConfig );
+    public AppScaleConfig getCluster ( String clusterName );
 
 
-    UUID uninstallCluster( AppScaleConfig appScaleConfig );
+    @Override
+    UUID installCluster ( AppScaleConfig appScaleConfig );
 
 
-    UUID startCluster( String clusterName );
+    UUID uninstallCluster ( AppScaleConfig appScaleConfig );
 
 
-    UUID stopCluster( String clusterName );
+    UUID startCluster ( String clusterName );
 
 
-    UUID restartCluster( String clusterName );
+    UUID stopCluster ( String clusterName );
 
 
-    UUID statusCluster( String clusterName );
+    UUID restartCluster ( String clusterName );
 
 
-    UUID startService( String clusterName, String hostName );
+    UUID statusCluster ( String clusterName );
 
 
-    UUID stopService( String clusterName, String hostName );
+    UUID startService ( String clusterName, String hostName );
 
 
-    UUID statusService( String clusterName, String hostName );
+    UUID stopService ( String clusterName, String hostName );
 
 
-    UUID addNode( String clusterName );
+    UUID statusService ( String clusterName, String hostName );
 
 
-    UUID destroyNode( String clusterName, String hostName );
+    UUID addNode ( String clusterName );
 
 
-    UUID removeCluster( String clusterName );
+    UUID destroyNode ( String clusterName, String hostName );
 
 
-    public ClusterSetupStrategy getClusterSetupStrategy( Environment e, TrackerOperation t, AppScaleConfig ac );
+    UUID removeCluster ( String clusterName );
 
 
-    public void saveCongig( AppScaleConfig ac );
+    public ClusterSetupStrategy getClusterSetupStrategy ( Environment e, TrackerOperation t, AppScaleConfig ac );
 
 
-    public void deleteConfig( AppScaleConfig ac );
+    public void saveCongig ( AppScaleConfig ac );
 
 
-    public AppScaleConfig getConfig( String clusterName );
+    public void deleteConfig ( AppScaleConfig ac );
+
+
+    public AppScaleConfig getConfig ( String clusterName );
 
 }
 
