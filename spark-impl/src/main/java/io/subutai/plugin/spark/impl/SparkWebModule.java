f99b0234a7ac41888110872f80839440def5c565
Talas Zholdoshbekov <tjoldoshbekov@critical-factor.com>
2016-01-18 12:40:27 +0200

    SS-4109 release RC6

--- spark-impl/src/main/java/io/subutai/plugin/spark/impl/SparkWebModule.java
+++ spark-impl/src/main/java/io/subutai/plugin/spark/impl/SparkWebModule.java
@@ -2,36 +2,47 @@ package io.subutai.plugin.spark.impl;
 
 import io.subutai.webui.api.WebuiModule;
 
-public class SparkWebModule implements WebuiModule
-{
+public class SparkWebModule implements WebuiModule {
     public static String NAME = "Spark";
     public static String IMG = "plugins/spark/spark.png";
 
     @Override
-    public String getName()
-    {
+    public String getName() {
         return NAME;
     }
 
 
     @Override
-    public String getModuleInfo()
-    {
-        return String.format( "{\"img\" : \"%s\", \"name\" : \"%s\"}", IMG, NAME );
+    public String getModuleInfo() {
+        return String.format("{\"img\" : \"%s\", \"name\" : \"%s\"}", IMG, NAME);
     }
 
 
     @Override
-    public String getAngularDependecyList()
-    {
-        return ".state('spark', {\n" + "url: '/plugins/spark',\n"
-                + "templateUrl: 'plugins/spark/partials/view.html',\n" + "resolve: {\n"
+    public String getAngularDependecyList() {
+        return ".state('spark', {\n"
+                + "url: '/plugins/spark',\n"
+                + "templateUrl: 'plugins/spark/partials/view.html',\n"
+                + "data: {\n"
+                + "     bodyClass: '',\n"
+                + "     layout: 'default'\n"
+                + "},\n"
+                + "resolve: {\n"
                 + "loadPlugin: ['$ocLazyLoad', function ($ocLazyLoad) {\n"
-                + "return $ocLazyLoad.load([\n" + "{\n"
-                + "name: 'subutai.plugins.spark',\n" + "files: [\n"
-                + "'plugins/spark/spark.js',\n" + "'plugins/spark/controller.js',\n"
-                + "'plugins/spark/service.js',\n" + "'plugins/hadoop/service.js',\n"
-                + "'subutai-app/environment/service.js'\n" + "]\n" + "}\n"
-                + "]);\n" + "}]\n" + "}\n" + "})";
+                + "return $ocLazyLoad.load([\n"
+                + "{\n"
+                + "name: 'subutai.plugins.spark',\n"
+                + "files: [\n"
+                + "'plugins/spark/spark.js',\n"
+                + "'plugins/spark/controller.js',\n"
+                + "'plugins/spark/service.js',\n"
+                + "'plugins/hadoop/service.js',\n"
+                + "'subutai-app/environment/service.js'\n"
+                + "]\n"
+                + "}\n"
+                + "]);\n"
+                + "}]\n"
+                + "}\n"
+                + "})";
     }
 }
