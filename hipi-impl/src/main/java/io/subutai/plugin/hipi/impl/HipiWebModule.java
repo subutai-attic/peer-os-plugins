788f36ef5d8d4fc1e841ce2f878a4409c00eed13
Kudaiar Pirimbaev <kpirimbaev@critical-factor.com>
2016-01-18 15:13:39 +0600

    SS-4116 Fixed "Refresh" bug

--- hipi-impl/src/main/java/io/subutai/plugin/hipi/impl/HipiWebModule.java
+++ hipi-impl/src/main/java/io/subutai/plugin/hipi/impl/HipiWebModule.java
@@ -28,6 +28,10 @@ public class HipiWebModule implements WebuiModule
         return ".state('hipi', {\n" +
                 "url: '/plugins/hipi',\n" +
                 "templateUrl: 'plugins/hipi/partials/view.html',\n" +
+				"data: {\n" +
+				"bodyClass: '',\n" +
+				"layout: 'default'\n" +
+				"},\n" +
                 "resolve: {\n" +
                 "loadPlugin: ['$ocLazyLoad', function ($ocLazyLoad) {\n" +
                 "return $ocLazyLoad.load([\n" +
