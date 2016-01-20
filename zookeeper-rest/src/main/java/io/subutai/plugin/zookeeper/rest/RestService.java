d04edbe5ab736b308226a861b8b2004670f40930
Kudaiar Pirimbaev <kpirimbaev@critical-factor.com>
2016-01-20 16:04:25 +0600

    SS-4119 Added About page

--- zookeeper-rest/src/main/java/io/subutai/plugin/zookeeper/rest/RestService.java
+++ zookeeper-rest/src/main/java/io/subutai/plugin/zookeeper/rest/RestService.java
@@ -140,4 +140,10 @@ public interface RestService
     @Path( "clusters/{clusterName}/available/nodes" )
     @Produces( { MediaType.TEXT_PLAIN } )
     public Response getAvailableNodes( @PathParam( "clusterName" ) String clusterName );
+
+
+	@GET
+	@Path( "about" )
+	@Produces( { MediaType.TEXT_PLAIN } )
+	public Response getPluginInfo();
 }
