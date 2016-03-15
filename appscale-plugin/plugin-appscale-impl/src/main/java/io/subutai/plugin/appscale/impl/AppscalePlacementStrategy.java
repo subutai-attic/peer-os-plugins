e51a2255ee9dccfa343ac0239a462ac9e78c7347
Timur Jamakeev <tjamakeev@critical-factor.com>
2016-03-15 18:23:33 +0600

    added appscale placement strategy

---
+++ appscale-plugin/plugin-appscale-impl/src/main/java/io/subutai/plugin/appscale/impl/AppscalePlacementStrategy.java
@@ -0,0 +1,167 @@
+package io.subutai.plugin.appscale.impl;
+
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.UUID;
+
+import io.subutai.common.environment.Node;
+import io.subutai.common.environment.NodeSchema;
+import io.subutai.common.environment.Topology;
+import io.subutai.common.peer.ContainerSize;
+import io.subutai.common.quota.ContainerQuota;
+import io.subutai.common.resource.PeerGroupResources;
+import io.subutai.common.resource.PeerResources;
+import io.subutai.core.strategy.api.ContainerPlacementStrategy;
+import io.subutai.core.strategy.api.StrategyException;
+
+
+/**
+ * Container placement strategy for appscale plugin
+ */
+public class AppscalePlacementStrategy implements ContainerPlacementStrategy
+{
+    private static final String APPSCALE_PLACEMENT_STRATEGY_ID = "APPSCALE_PLACEMENT_STRATEGY";
+
+
+    @Override
+    public String getId()
+    {
+        return APPSCALE_PLACEMENT_STRATEGY_ID;
+    }
+
+
+    @Override
+    public String getTitle()
+    {
+        return "This default strategy for appscale plugin.";
+    }
+
+
+    @Override
+    public List<NodeSchema> getScheme()
+    {
+        final List<NodeSchema> result = new ArrayList<>();
+        NodeSchema schema =
+                new NodeSchema( "appscale-" + UUID.randomUUID().toString(), ContainerSize.SMALL, "appscale", 0, 0 );
+
+        result.add( schema );
+
+        return result;
+    }
+
+
+    @Override
+    public Topology distribute( final String environmentName, PeerGroupResources peerGroupResources,
+                                Map<ContainerSize, ContainerQuota> quotas ) throws StrategyException
+    {
+        Topology result = new Topology( environmentName );
+
+        Set<Node> nodes = distribute( getScheme(), peerGroupResources, quotas );
+        for ( Node node : nodes )
+        {
+            result.addNodePlacement( node.getPeerId(), node );
+        }
+
+        return result;
+    }
+
+
+    @Override
+    public Topology distribute( final String environmentName, final List<NodeSchema> nodeSchema,
+                                final PeerGroupResources peerGroupResources,
+                                final Map<ContainerSize, ContainerQuota> quotas ) throws StrategyException
+    {
+        Topology result = new Topology( environmentName );
+
+        Set<Node> ng = distribute( nodeSchema, peerGroupResources, quotas );
+        for ( Node node : ng )
+        {
+            result.addNodePlacement( node.getPeerId(), node );
+        }
+
+        return result;
+    }
+
+    protected Set<Node> distribute( List<NodeSchema> nodeSchemas, PeerGroupResources peerGroupResources,
+                                    Map<ContainerSize, ContainerQuota> quotas ) throws StrategyException
+    {
+
+        // build list of allocators
+        List<Allocator> allocators = new ArrayList<>();
+        for ( PeerResources peerResources : peerGroupResources.getResources() )
+        {
+            Allocator resourceAllocator = new Allocator( peerResources );
+            allocators.add( resourceAllocator );
+        }
+
+        if ( allocators.size() < 1 )
+        {
+            throw new StrategyException( "There are no resource hosts to place containers." );
+        }
+
+        // distribute node groups
+        for ( NodeSchema nodeSchema : nodeSchemas )
+        {
+            String containerName = generateContainerName( nodeSchema );
+
+            // select preferred peer
+            List<Allocator> preferredAllocators = getPreferredAllocators( allocators );
+            boolean allocated = false;
+            for ( Allocator resourceAllocator : preferredAllocators )
+            {
+                allocated = resourceAllocator
+                        .allocate( containerName, nodeSchema.getTemplateName(), nodeSchema.getSize(),
+                                quotas.get( nodeSchema.getSize() ) );
+                if ( allocated )
+                {
+                    break;
+                }
+            }
+
+            if ( !allocated )
+            {
+                throw new StrategyException(
+                        "Could not allocate containers. There is no space for container: '" + containerName + "'" );
+            }
+        }
+
+        Set<Node> nodes = new HashSet<>();
+
+        for ( Allocator resourceAllocator : allocators )
+        {
+            List<Allocator.AllocatedContainer> containers = resourceAllocator.getContainers();
+            if ( !containers.isEmpty() )
+            {
+                for ( Allocator.AllocatedContainer container : containers )
+                {
+                    Node node =
+                            new Node( UUID.randomUUID().toString(), container.getName(), container.getTemplateName(),
+                                    container.getSize(), 0, 0, container.getPeerId(), container.getHostId() );
+                    nodes.add( node );
+                }
+            }
+        }
+
+
+        return nodes;
+    }
+
+
+    private List<Allocator> getPreferredAllocators( final List<Allocator> allocators )
+    {
+        List<Allocator> result = new ArrayList<>( allocators );
+        Collections.shuffle( result );
+        return result;
+    }
+
+
+    private String generateContainerName( final NodeSchema nodeSchema )
+    {
+        return nodeSchema.getName().replaceAll( "\\s+", "_" );
+    }
+}
