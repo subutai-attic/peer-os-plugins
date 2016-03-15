26dec732ccbfcb2c80d1cfc2d36f8231cc847056
Timur Jamakeev <tjamakeev@critical-factor.com>
2016-03-15 18:25:23 +0600

    implemented action when /home partition stressed

--- appscale-plugin/plugin-appscale-impl/src/main/java/io/subutai/plugin/appscale/impl/handler/AlertHandler.java
+++ appscale-plugin/plugin-appscale-impl/src/main/java/io/subutai/plugin/appscale/impl/handler/AlertHandler.java
@@ -1,20 +1,42 @@
 package io.subutai.plugin.appscale.impl.handler;
 
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
+import java.util.UUID;
+import java.util.concurrent.CopyOnWriteArraySet;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import io.subutai.common.command.CommandUtil;
+import io.subutai.common.command.RequestBuilder;
 import io.subutai.common.environment.Environment;
+import io.subutai.common.environment.EnvironmentModificationException;
+import io.subutai.common.environment.EnvironmentNotFoundException;
+import io.subutai.common.environment.NodeSchema;
+import io.subutai.common.environment.Topology;
 import io.subutai.common.metric.QuotaAlertValue;
 import io.subutai.common.peer.AlertHandlerException;
+import io.subutai.common.peer.ContainerSize;
 import io.subutai.common.peer.EnvironmentContainerHost;
 import io.subutai.common.peer.ExceededQuotaAlertHandler;
+import io.subutai.common.peer.Peer;
+import io.subutai.common.peer.PeerException;
+import io.subutai.common.quota.ContainerQuota;
+import io.subutai.common.resource.ByteValueResource;
+import io.subutai.common.resource.ContainerResourceType;
+import io.subutai.common.resource.PeerGroupResources;
+import io.subutai.common.resource.PeerResources;
+import io.subutai.common.task.CloneRequest;
+import io.subutai.core.strategy.api.StrategyException;
 import io.subutai.plugin.appscale.api.AppScaleConfig;
 import io.subutai.plugin.appscale.impl.AppScaleImpl;
+import io.subutai.plugin.appscale.impl.AppscalePlacementStrategy;
 
 
 /**
@@ -25,6 +47,7 @@ public class AlertHandler extends ExceededQuotaAlertHandler
     private static final Logger LOG = LoggerFactory.getLogger( AlertHandler.class );
     private static final String HANDLER_ID = "DEFAULT_APPSCALE_QUOTA_EXCEEDED_ALERT_HANDLER";
     private AppScaleImpl appScale;
+    private static Set<String> locks = new CopyOnWriteArraySet<>();
 
 
     public AlertHandler( final AppScaleImpl appScale )
@@ -62,7 +85,6 @@ public class AlertHandler extends ExceededQuotaAlertHandler
         final List<AppScaleConfig> clusters = appScale.getClusters();
 
         String environmentId = environment.getId();
-        String sourceHostId = alertValue.getValue().getHostId().getId();
 
         AppScaleConfig targetCluster = null;
         for ( AppScaleConfig cluster : clusters )
@@ -80,20 +102,7 @@ public class AlertHandler extends ExceededQuotaAlertHandler
             return;
         }
 
-        EnvironmentContainerHost sourceHost = null;
-
-        //get environment containers and find alert's source host
-        Set<EnvironmentContainerHost> containers = environment.getContainerHosts();
-
-        for ( EnvironmentContainerHost containerHost : containers )
-        {
-            if ( containerHost.getId().equalsIgnoreCase( sourceHostId ) )
-            {
-                sourceHost = containerHost;
-                break;
-            }
-        }
-
+        EnvironmentContainerHost sourceHost = getSourceHost();
 
         if ( sourceHost == null )
         {
@@ -109,6 +118,116 @@ public class AlertHandler extends ExceededQuotaAlertHandler
                     alertValue.getValue().getHostId() ) );
             return;
         }
+
+
+        if ( alertValue.getValue().getContainerResourceType() == ContainerResourceType.HOME )
+        {
+            final ByteValueResource current = alertValue.getValue().getCurrentValue( ByteValueResource.class );
+            final ByteValueResource quota = alertValue.getValue().getQuotaValue( ByteValueResource.class );
+            if ( current == null || quota == null )
+            {
+                // invalid value
+                return;
+            }
+
+            boolean isStressed = quota.getValue().compareTo( current.getValue() ) < 1
+                    || quota.getValue().multiply( new BigDecimal( "0.8" ) ).compareTo( current.getValue() ) < 1;
+
+
+            if ( isStressed )
+            {
+                createNewInstance( environment );
+            }
+        }
+    }
+
+
+    private void createNewInstance( Environment environment )
+    {
+        if ( isLocked( environment.getId() ) )
+        {
+            LOG.debug( "Environment is locked. Skipping." );
+        }
+
+        try
+        {
+            lock( environment.getId() );
+
+            final PeerGroupResources peerGroupResources = appScale.getPeerManager().getPeerGroupResources();
+            final Map<ContainerSize, ContainerQuota> quotas = appScale.getQuotaManager().getDefaultQuotas();
+
+            final List<PeerResources> resources = new ArrayList<>();
+            final Set<String> preferredPeers = getPreferredPeers( environment );
+
+            for ( final PeerResources peerResources : peerGroupResources.getResources() )
+            {
+                if ( preferredPeers.contains( peerResources.getPeerId() ) )
+                {
+                    resources.add( peerResources );
+                }
+            }
+
+            PeerGroupResources neighbors = new PeerGroupResources( resources, peerGroupResources.getDistances() );
+
+            Topology topology = null;
+            try
+            {
+                topology = new AppscalePlacementStrategy().distribute( environment.getName(), neighbors, quotas );
+            }
+            catch ( StrategyException e )
+            {
+                // ignore
+            }
+
+            if ( topology == null )
+            {
+                topology =
+                        new AppscalePlacementStrategy().distribute( environment.getName(), peerGroupResources, quotas );
+            }
+
+            final Set<EnvironmentContainerHost> result =
+                    appScale.getEnvironmentManager().growEnvironment( environment.getId(), topology, false );
+
+            // grow succeeded
+            //TODO: need config new appscale instance
+        }
+        catch ( PeerException | EnvironmentModificationException | EnvironmentNotFoundException | StrategyException e )
+        {
+            LOG.error( e.getMessage(), e );
+        }
+        finally
+        {
+            unlock( environment.getId() );
+        }
+    }
+
+
+    private Set<String> getPreferredPeers( final Environment environment )
+    {
+        final Set<String> result = new HashSet<>();
+        for ( EnvironmentContainerHost c : environment.getContainerHosts() )
+        {
+            result.add( c.getPeerId() );
+        }
+        return result;
+    }
+
+
+    private void unlock( final String environmentId )
+    {
+        locks.remove( environmentId );
+    }
+
+
+    private void lock( final String environmentId )
+    {
+        locks.add( environmentId );
+    }
+
+
+    private boolean isLocked( final String environmentId )
+    {
+        return locks.contains( environmentId );
     }
 }
 
