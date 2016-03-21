154c314e78f2741566c48d8952d83baacf648efe
Timur Jamakeev <tjamakeev@critical-factor.com>
2016-03-21 21:04:58 +0600

    container placement strategy changed

--- appscale-plugin/plugin-appscale-impl/src/main/java/io/subutai/plugin/appscale/impl/AppscalePlacementStrategy.java
+++ appscale-plugin/plugin-appscale-impl/src/main/java/io/subutai/plugin/appscale/impl/AppscalePlacementStrategy.java
@@ -4,10 +4,13 @@ package io.subutai.plugin.appscale.impl;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.UUID;
+
+import com.google.common.collect.Iterators;
 
 import io.subutai.common.environment.Node;
 import io.subutai.common.environment.NodeSchema;
@@ -32,42 +35,42 @@ public class AppscalePlacementStrategy implements ContainerPlacementStrategy
 
 
     @Override
-    public String getId ()
+    public String getId()
     {
         return APPSCALE_PLACEMENT_STRATEGY_ID;
     }
 
 
     @Override
-    public String getTitle ()
+    public String getTitle()
     {
         return "This default strategy for appscale plugin.";
     }
 
 
     @Override
-    public List<NodeSchema> getScheme ()
+    public List<NodeSchema> getScheme()
     {
-        final List<NodeSchema> result = new ArrayList<> ();
-        NodeSchema schema
-                = new NodeSchema ( "appscale-" + UUID.randomUUID ().toString (), ContainerSize.HUGE, "appscale", 0, 0 );
+        final List<NodeSchema> result = new ArrayList<>();
+        NodeSchema schema =
+                new NodeSchema( "appscale-" + UUID.randomUUID().toString(), ContainerSize.HUGE, "appscale", 0, 0 );
 
-        result.add ( schema );
+        result.add( schema );
 
         return result;
     }
 
 
     @Override
-    public Topology distribute ( final String environmentName, PeerGroupResources peerGroupResources,
-                                 Map<ContainerSize, ContainerQuota> quotas ) throws StrategyException
+    public Topology distribute( final String environmentName, PeerGroupResources peerGroupResources,
+                                Map<ContainerSize, ContainerQuota> quotas ) throws StrategyException
     {
-        Topology result = new Topology ( environmentName );
+        Topology result = new Topology( environmentName );
 
-        Set<Node> nodes = distribute ( getScheme (), peerGroupResources, quotas );
+        Set<Node> nodes = distribute( getScheme(), peerGroupResources, quotas );
         for ( Node node : nodes )
         {
-            result.addNodePlacement ( node.getPeerId (), node );
+            result.addNodePlacement( node.getPeerId(), node );
         }
 
         return result;
@@ -75,79 +78,80 @@ public class AppscalePlacementStrategy implements ContainerPlacementStrategy
 
 
     @Override
-    public Topology distribute ( final String environmentName, final List<NodeSchema> nodeSchema,
-                                 final PeerGroupResources peerGroupResources,
-                                 final Map<ContainerSize, ContainerQuota> quotas ) throws StrategyException
+    public Topology distribute( final String environmentName, final List<NodeSchema> nodeSchema,
+                                final PeerGroupResources peerGroupResources,
+                                final Map<ContainerSize, ContainerQuota> quotas ) throws StrategyException
     {
-        Topology result = new Topology ( environmentName );
+        Topology result = new Topology( environmentName );
 
-        Set<Node> ng = distribute ( nodeSchema, peerGroupResources, quotas );
+        Set<Node> ng = distribute( nodeSchema, peerGroupResources, quotas );
         for ( Node node : ng )
         {
-            result.addNodePlacement ( node.getPeerId (), node );
+            result.addNodePlacement( node.getPeerId(), node );
         }
 
         return result;
     }
 
 
-    protected Set<Node> distribute ( List<NodeSchema> nodeSchemas, PeerGroupResources peerGroupResources,
-                                     Map<ContainerSize, ContainerQuota> quotas ) throws StrategyException
+    protected Set<Node> distribute( List<NodeSchema> nodeSchemas, PeerGroupResources peerGroupResources,
+                                    Map<ContainerSize, ContainerQuota> quotas ) throws StrategyException
     {
 
         // build list of allocators
-        List<Allocator> allocators = new ArrayList<> ();
-        for ( PeerResources peerResources : peerGroupResources.getResources () )
+        List<Allocator> allocators = new ArrayList<>();
+        for ( PeerResources peerResources : peerGroupResources.getResources() )
         {
-            Allocator resourceAllocator = new Allocator ( peerResources );
-            allocators.add ( resourceAllocator );
+            Allocator resourceAllocator = new Allocator( peerResources );
+            allocators.add( resourceAllocator );
         }
 
-        if ( allocators.size () < 1 )
+        if ( allocators.size() < 1 )
         {
-            throw new StrategyException ( "There are no resource hosts to place containers." );
+            throw new StrategyException( "There are no resource hosts to place containers." );
         }
 
+        final Iterator<Allocator> iterator = Iterators.cycle( allocators );
         // distribute node groups
         for ( NodeSchema nodeSchema : nodeSchemas )
         {
-            String containerName = generateContainerName ( nodeSchema );
+            String containerName = generateContainerName( nodeSchema );
 
-            // select preferred peer
-            List<Allocator> preferredAllocators = getPreferredAllocators ( allocators );
             boolean allocated = false;
-            for ( Allocator resourceAllocator : preferredAllocators )
+            int counter = 0;
+            while ( counter < allocators.size() )
             {
+                final Allocator resourceAllocator = iterator.next();
                 allocated = resourceAllocator
-                        .allocate ( containerName, nodeSchema.getTemplateName (), nodeSchema.getSize (),
-                                    quotas.get ( nodeSchema.getSize () ) );
+                        .allocate( containerName, nodeSchema.getTemplateName(), nodeSchema.getSize(),
+                                quotas.get( nodeSchema.getSize() ) );
                 if ( allocated )
                 {
                     break;
                 }
+                counter++;
             }
 
             if ( !allocated )
             {
-                throw new StrategyException (
+                throw new StrategyException(
                         "Could not allocate containers. There is no space for container: '" + containerName + "'" );
             }
         }
 
-        Set<Node> nodes = new HashSet<> ();
+        Set<Node> nodes = new HashSet<>();
 
         for ( Allocator resourceAllocator : allocators )
         {
-            List<Allocator.AllocatedContainer> containers = resourceAllocator.getContainers ();
-            if ( !containers.isEmpty () )
+            List<Allocator.AllocatedContainer> containers = resourceAllocator.getContainers();
+            if ( !containers.isEmpty() )
             {
                 for ( Allocator.AllocatedContainer container : containers )
                 {
-                    Node node
-                            = new Node ( UUID.randomUUID ().toString (), container.getName (),
-                                         container.getTemplateName (),
-                                         container.getSize (), 0, 0, container.getPeerId (), container.getHostId () );
-                    nodes.add ( node );
+                    Node node =
+                            new Node( UUID.randomUUID().toString(), container.getName(), container.getTemplateName(),
+                                    container.getSize(), 0, 0, container.getPeerId(), container.getHostId() );
+                    nodes.add( node );
                 }
             }
         }
@@ -157,17 +161,17 @@ public class AppscalePlacementStrategy implements ContainerPlacementStrategy
     }
 
 
-    private List<Allocator> getPreferredAllocators ( final List<Allocator> allocators )
+    private List<Allocator> getPreferredAllocators( final List<Allocator> allocators )
     {
-        List<Allocator> result = new ArrayList<> ( allocators );
-        Collections.shuffle ( result );
+        List<Allocator> result = new ArrayList<>( allocators );
+        Collections.shuffle( result );
         return result;
     }
 
 
-    private String generateContainerName ( final NodeSchema nodeSchema )
+    private String generateContainerName( final NodeSchema nodeSchema )
     {
-        return nodeSchema.getName ().replaceAll ( "\\s+", "_" );
+        return nodeSchema.getName().replaceAll( "\\s+", "_" );
     }
 }
 
--- appscale-plugin/plugin-appscale-impl/src/main/java/io/subutai/plugin/appscale/impl/handler/AppscaleAlertHandler.java
+++ appscale-plugin/plugin-appscale-impl/src/main/java/io/subutai/plugin/appscale/impl/handler/AppscaleAlertHandler.java
@@ -43,52 +43,52 @@ import io.subutai.plugin.appscale.impl.AppscalePlacementStrategy;
  */
 public class AppscaleAlertHandler extends ExceededQuotaAlertHandler
 {
-    private static final Logger LOG = LoggerFactory.getLogger ( AppscaleAlertHandler.class );
+    private static final Logger LOG = LoggerFactory.getLogger( AppscaleAlertHandler.class );
     public static final String HANDLER_ID = "DEFAULT_APPSCALE_QUOTA_EXCEEDED_ALERT_HANDLER";
     private AppScaleImpl appScale;
-    private static Set<String> locks = new CopyOnWriteArraySet<> ();
+    private static Set<String> locks = new CopyOnWriteArraySet<>();
 
 
-    public AppscaleAlertHandler ( final AppScaleImpl appScale )
+    public AppscaleAlertHandler( final AppScaleImpl appScale )
     {
         this.appScale = appScale;
     }
 
 
-    private void throwAlertException ( String context, Exception e ) throws AlertHandlerException
+    private void throwAlertException( String context, Exception e ) throws AlertHandlerException
     {
-        LOG.error ( context, e );
-        throw new AlertHandlerException ( context, e );
+        LOG.error( context, e );
+        throw new AlertHandlerException( context, e );
     }
 
 
     @Override
-    public String getId ()
+    public String getId()
     {
         return HANDLER_ID;
     }
 
 
     @Override
-    public String getDescription ()
+    public String getDescription()
     {
         return "Node resource threshold excess default alert handler for appScale.";
     }
 
 
     @Override
-    public void process ( final Environment environment, final QuotaAlertValue alertValue ) throws AlertHandlerException
+    public void process( final Environment environment, final QuotaAlertValue alertValue ) throws AlertHandlerException
     {
-        LOG.debug ( String.format ( "%s", alertValue ) );
+        LOG.debug( String.format( "%s", alertValue ) );
         //find appScale cluster by environment id
-        final List<AppScaleConfig> clusters = appScale.getClusters ();
+        final List<AppScaleConfig> clusters = appScale.getClusters();
 
-        String environmentId = environment.getId ();
+        String environmentId = environment.getId();
 
         AppScaleConfig targetCluster = null;
         for ( AppScaleConfig cluster : clusters )
         {
-            if ( cluster.getEnvironmentId ().equals ( environmentId ) )
+            if ( cluster.getEnvironmentId().equals( environmentId ) )
             {
                 targetCluster = cluster;
                 break;
@@ -97,16 +97,16 @@ public class AppscaleAlertHandler extends ExceededQuotaAlertHandler
 
         if ( targetCluster == null )
         {
-            throwAlertException ( String.format ( "Cluster not found by environment id %s", environmentId ), null );
+            throwAlertException( String.format( "Cluster not found by environment id %s", environmentId ), null );
             return;
         }
 
-        EnvironmentContainerHost sourceHost = getSourceHost ();
+        EnvironmentContainerHost sourceHost = getSourceHost();
 
         if ( sourceHost == null )
         {
-            throwAlertException ( String.format ( "Alert source host %s not found in environment",
-                                                  alertValue.getValue ().getHostId ().getId () ), null );
+            throwAlertException( String.format( "Alert source host %s not found in environment",
+                    alertValue.getValue().getHostId().getId() ), null );
             return;
         }
 
@@ -118,186 +118,190 @@ public class AppscaleAlertHandler extends ExceededQuotaAlertHandler
         //            return;
         //        }
 
-        if ( isAppengineStressed ( alertValue.getValue () ) )
+        if ( isAppengineStressed( alertValue.getValue() ) )
         {
-            createAppEngineInstance ( environment, targetCluster, sourceHost );
+            createAppEngineInstance( environment, targetCluster, sourceHost );
         }
-
     }
 
 
-    private void createAppEngineInstance ( Environment environment, AppScaleConfig config,
-                                           EnvironmentContainerHost sourceHost )
+    private void createAppEngineInstance( Environment environment, AppScaleConfig config,
+                                          EnvironmentContainerHost sourceHost )
     {
-        if ( isLocked ( environment.getId () ) )
+        if ( isLocked( environment.getId() ) )
         {
-            LOG.debug ( "Environment is locked. Skipping." );
+            LOG.debug( "Environment is locked. Skipping." );
         }
 
         try
         {
-            lock ( environment.getId () );
+            lock( environment.getId() );
 
-            final PeerGroupResources peerGroupResources = appScale.getPeerManager ().getPeerGroupResources ();
-            final Map<ContainerSize, ContainerQuota> quotas = appScale.getQuotaManager ().getDefaultQuotas ();
+            final PeerGroupResources peerGroupResources = appScale.getPeerManager().getPeerGroupResources();
+            final Map<ContainerSize, ContainerQuota> quotas = appScale.getQuotaManager().getDefaultQuotas();
 
-            final List<PeerResources> resources = new ArrayList<> ();
-            final Set<String> preferredPeers = getPreferredPeers ( environment );
+            final List<PeerResources> resources = new ArrayList<>();
+            final List<String> preferredPeerList = getPreferredPeers( environment, peerGroupResources );
 
-            for ( final PeerResources peerResources : peerGroupResources.getResources () )
+            for ( String peerId : preferredPeerList )
             {
-                if ( preferredPeers.contains ( peerResources.getPeerId () ) )
-                {
-                    resources.add ( peerResources );
-                }
+                PeerResources peerResources = findResource( peerGroupResources.getResources(), peerId );
+                resources.add( peerResources );
             }
 
-            PeerGroupResources neighbors = new PeerGroupResources ( resources, peerGroupResources.getDistances () );
+            PeerGroupResources preferredPeers = new PeerGroupResources( resources, peerGroupResources.getDistances() );
 
-            final List<NodeSchema> nodes = new ArrayList<> ();
-            String newAppengineName = "appengine-" + UUID.randomUUID ().toString ();
-            LOG.info ( "NEW APPENGINE NAME::::::::::" + newAppengineName );
-            nodes.add (
-                    new NodeSchema ( newAppengineName, ContainerSize.HUGE, "appscale", 0,
-                                     0 ) );
-            Topology topology = null;
-            try
-            {
+            final List<NodeSchema> nodes = new ArrayList<>();
+            String newAppengineName = "appengine-" + UUID.randomUUID().toString();
+            LOG.info( "NEW APPENGINE NAME::::::::::" + newAppengineName );
+            nodes.add( new NodeSchema( newAppengineName, ContainerSize.HUGE, "appscale", 0, 0 ) );
+            final AppscalePlacementStrategy appscalePlacementStrategy = new AppscalePlacementStrategy();
 
-                topology
-                        = new AppscalePlacementStrategy ().distribute ( environment.getName (), nodes, neighbors, quotas );
-            }
-            catch ( StrategyException e )
-            {
-                // ignore
-            }
+            Topology topology =
+                    appscalePlacementStrategy.distribute( environment.getName(), nodes, preferredPeers, quotas );
 
-            if ( topology == null )
-            {
-                topology = new AppscalePlacementStrategy ()
-                        .distribute ( environment.getName (), nodes, peerGroupResources, quotas );
-            }
-
-            final Set<EnvironmentContainerHost>[] result = new Set[]
-            {
-                null
+            final Set<EnvironmentContainerHost>[] result = new Set[] {
+                    null
             };
 
-//            final Session session = appScale.getIdentityManager().login( "internal", "secretSubutai" );
-            final Session session = appScale.getIdentityManager ().login ( "admin", "secret" );
+            //            final Session session = appScale.getIdentityManager().login( "internal", "secretSubutai" );
+            final Session session = appScale.getIdentityManager().login( "admin", "secret" );
             final Topology finalTopology = topology;
-            Subject.doAs ( session.getSubject (), new PrivilegedAction<Void> ()
-                   {
-                       @Override
-                       public Void run ()
-                       {
-                           try
-                           {
-                               result[0] = appScale.getEnvironmentManager ()
-                                       .growEnvironment ( environment.getId (), finalTopology, false );
-                           }
-                           catch ( Exception ex )
-                           {
-                               LOG.error ( ex.getMessage (), ex );
-                           }
-                           return null;
-                       }
-                   } );
+            Subject.doAs( session.getSubject(), new PrivilegedAction<Void>()
+            {
+                @Override
+                public Void run()
+                {
+                    try
+                    {
+                        result[0] = appScale.getEnvironmentManager()
+                                            .growEnvironment( environment.getId(), finalTopology, false );
+                    }
+                    catch ( Exception ex )
+                    {
+                        LOG.error( ex.getMessage(), ex );
+                    }
+                    return null;
+                }
+            } );
 
-            LOG.debug ( String.format ( "%s", result[0] ) );
+            LOG.debug( String.format( "%s", result[0] ) );
 
-            Iterator<EnvironmentContainerHost> iterator = result[0].iterator ();
-            EnvironmentContainerHost next = iterator.next ();
-            LOG.info ( "CONTAINER NAME: " + next.getHostname () );
+            Iterator<EnvironmentContainerHost> iterator = result[0].iterator();
+            EnvironmentContainerHost next = iterator.next();
+            LOG.info( "CONTAINER NAME: " + next.getHostname() );
             // grow succeeded
             //TODO: need config modified appscale
-            List<String> appenList = config.getAppenList ();
-            appenList.add ( next.getHostname () );
-            config.setAppenList ( appenList ); // new appengine setted...
-            Boolean modifiyConfig = modifiyConfig ( environment, config );
+            List<String> appenList = config.getAppenList();
+            appenList.add( next.getHostname() );
+            config.setAppenList( appenList ); // new appengine setted...
+            Boolean modifiyConfig = modifiyConfig( environment, config );
             if ( modifiyConfig )
             {
-                LOG.info ( "Appscale is scaled up successfully" );
+                LOG.info( "Appscale is scaled up successfully" );
             }
             else
             {
-                LOG.error ( "Appscale scale up failed" );
+                LOG.error( "Appscale scale up failed" );
             }
         }
         catch ( Exception e )
         {
-            LOG.error ( e.getMessage (), e );
+            LOG.error( e.getMessage(), e );
         }
         finally
         {
-            unlock ( environment.getId () );
+            unlock( environment.getId() );
         }
     }
 
 
-    /**
-     *
-     * @param environment
-     * @param config
-     * @return
-     *
-     * actuall configuration for the config file in appscale
-     *
-     */
-    private Boolean modifiyConfig ( Environment environment, AppScaleConfig targetCluster )
+    private PeerResources findResource( final List<PeerResources> resources, final String peerId )
     {
-        Boolean modifed = false;
-        UUID addNode = appScale.addNode ( targetCluster );
-        return modifed;
-    }
-
-
-    private Set<String> getPreferredPeers ( final Environment environment )
-    {
-        final Set<String> result = new HashSet<> ();
-        for ( EnvironmentContainerHost c : environment.getContainerHosts () )
+        PeerResources result = null;
+        Iterator<PeerResources> i = resources.iterator();
+        while ( result == null && i.hasNext() )
         {
-            result.add ( c.getPeerId () );
+            PeerResources r = i.next();
+            if ( peerId.equals( r.getPeerId() ) )
+            {
+                result = r;
+            }
         }
         return result;
     }
 
 
-    private void unlock ( final String environmentId )
+    /**
+     * @return actuall configuration for the config file in appscale
+     */
+    private Boolean modifiyConfig( Environment environment, AppScaleConfig targetCluster )
     {
-        locks.remove ( environmentId );
+        Boolean modifed = false;
+        UUID addNode = appScale.addNode( targetCluster );
+        return modifed;
     }
 
 
-    private void lock ( final String environmentId )
+    private List<String> getPreferredPeers( final Environment environment, final PeerGroupResources peerGroupResources )
     {
-        locks.add ( environmentId );
-    }
+        final Set<String> usedPeers = new HashSet<>();
 
-
-    private boolean isLocked ( final String environmentId )
-    {
-        return locks.contains ( environmentId );
-    }
-
-
-    public boolean isAppengineStressed ( final ExceededQuota value )
-    {
-        EnvironmentContainerHost host = getSourceHost ();
-
-        boolean isAppengine = isAppengineInstance ( host );
-        if ( isAppengine && value.getContainerResourceType () == ContainerResourceType.HOME )
+        for ( EnvironmentContainerHost c : environment.getContainerHosts() )
         {
-            final ByteValueResource current = value.getCurrentValue ( ByteValueResource.class );
-            final ByteValueResource quota = value.getQuotaValue ( ByteValueResource.class );
+            usedPeers.add( c.getPeerId() );
+        }
+
+        Set<String> notUsedPeers = new HashSet<>();
+        for ( PeerResources resources : peerGroupResources.getResources() )
+        {
+            if ( !usedPeers.contains( resources.getPeerId() ) )
+            {
+                notUsedPeers.add( resources.getPeerId() );
+            }
+        }
+        final List<String> result = new ArrayList<>();
+        result.addAll( notUsedPeers );
+        result.addAll( usedPeers );
+        return result;
+    }
+
+
+    private void unlock( final String environmentId )
+    {
+        locks.remove( environmentId );
+    }
+
+
+    private void lock( final String environmentId )
+    {
+        locks.add( environmentId );
+    }
+
+
+    private boolean isLocked( final String environmentId )
+    {
+        return locks.contains( environmentId );
+    }
+
+
+    public boolean isAppengineStressed( final ExceededQuota value )
+    {
+        EnvironmentContainerHost host = getSourceHost();
+
+        boolean isAppengine = isAppengineInstance( host );
+        if ( isAppengine && value.getContainerResourceType() == ContainerResourceType.HOME )
+        {
+            final ByteValueResource current = value.getCurrentValue( ByteValueResource.class );
+            final ByteValueResource quota = value.getQuotaValue( ByteValueResource.class );
             if ( current == null || quota == null )
             {
                 // invalid value
                 return false;
             }
 
-            boolean stressed = quota.getValue ().compareTo ( current.getValue () ) < 1
-                    || quota.getValue ().multiply ( new BigDecimal ( "0.8" ) ).compareTo ( current.getValue () ) < 1;
+            boolean stressed = quota.getValue().compareTo( current.getValue() ) < 1
+                    || quota.getValue().multiply( new BigDecimal( "0.8" ) ).compareTo( current.getValue() ) < 1;
 
             // appengine instance and stressed /home partition
             return stressed;
@@ -307,7 +311,7 @@ public class AppscaleAlertHandler extends ExceededQuotaAlertHandler
     }
 
 
-    private boolean isAppengineInstance ( final EnvironmentContainerHost host )
+    private boolean isAppengineInstance( final EnvironmentContainerHost host )
     {
         // todo: is this host appengine instance?
         // while just return true
