dd77e71982e359efa586a7e2e8fed0f5b5b974a2
larenso <istuts@optimal-dynamics.com>
2016-04-20 18:43:34 +0600

    added missing dependencies

--- usergrid-plugin/impl/src/main/java/io/subutai/plugin/usergrid/impl/UsergridWebModule.java
+++ usergrid-plugin/impl/src/main/java/io/subutai/plugin/usergrid/impl/UsergridWebModule.java
@@ -1,39 +1,20 @@
 package io.subutai.plugin.usergrid.impl;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import com.google.gson.Gson;
+
 import io.subutai.webui.api.WebuiModule;
+import io.subutai.webui.entity.AngularjsDependency;
+import io.subutai.webui.entity.WebuiModuleResourse;
+
 
 public class UsergridWebModule implements WebuiModule
 {
 	public static String NAME = "Usergrid";
 	public static String IMG = "plugins/usergrid/usergrid.png";
 	private static final Map<String, Integer> TEMPLATES_REQUIREMENT;
-	@Override
-	public String getAngularState ()
-	{
-		return appscaleResource.getAngularjsList ();
-	}
-
-
-	@Override
-	public String getName ()
-	{
-		return NAME;
-	}
-
-
-	@Override
-	public String getModuleInfo ()
-	{
-		return String.format ( "{\"img\" : \"%s\", \"name\" : \"%s\", \"requirement\" : %s}", IMG, NAME,
-				new Gson ().toJson ( TEMPLATES_REQUIREMENT ).toString () );
-	}
-
-
-	@Override
-	public String getAngularDependecyList ()
-	{
-		return String.format ( ".state('%s', %s)", NAME.toLowerCase(), appscaleResource.getAngularjsList () );
-	}
 
 	static
 	{
