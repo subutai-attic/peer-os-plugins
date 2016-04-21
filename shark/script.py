abdc188ef295022ef5dded553668be7f414e2816
larcohex <kpirimbaev@optimal-dynamics.com>
2016-04-21 19:55:19 +0600

    * [ALL] Fixed features.xml for latest release

--- shark/script.py
+++ shark/script.py
@@ -37,7 +37,7 @@ if skip_test != "y" and skip_test != "n":
 command = "mvn clean install "
 if skip_test == "y":
 	command += "-Dmaven.test.skip=true "
-command += "&& scp " + plugin + "-api/target/" + plugin + "-plugin-api-4.0.0-RC9-SNAPSHOT.jar " + plugin + "-cli/target/" + plugin + "-plugin-cli-4.0.0-RC9-SNAPSHOT.jar " + plugin + "-impl/target/" + plugin + "-plugin-impl-4.0.0-RC9-SNAPSHOT.jar " + plugin + "-rest/target/" + plugin + "-plugin-rest-4.0.0-RC9-SNAPSHOT.jar "
+command += "&& scp " + plugin + "-api/target/" + plugin + "-plugin-api-4.0.0-RC10-SNAPSHOT.jar " + plugin + "-cli/target/" + plugin + "-plugin-cli-4.0.0-RC10-SNAPSHOT.jar " + plugin + "-impl/target/" + plugin + "-plugin-impl-4.0.0-RC10-SNAPSHOT.jar " + plugin + "-rest/target/" + plugin + "-plugin-rest-4.0.0-RC10-SNAPSHOT.jar "
 command += "root@management" + n + ".critical-factor.com:/root/" + path + "\n"
 script = open ("build.sh", "w+")
 script.write (command)
--- spark/build.sh
+++ spark/build.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-mvn clean install && scp spark-api/target/spark-plugin-api-4.0.0-RC9-SNAPSHOT.jar spark-cli/target/spark-plugin-cli-4.0.0-RC9-SNAPSHOT.jar spark-impl/target/spark-plugin-impl-4.0.0-RC9-SNAPSHOT.jar spark-rest/target/spark-plugin-rest-4.0.0-RC9-SNAPSHOT.jar root@management4.critical-factor.com:/root/kudayar/subutai-4.0.0-RC9-SNAPSHOT/deploy
+mvn clean install && scp spark-api/target/spark-plugin-api-4.0.0-RC10-SNAPSHOT.jar spark-cli/target/spark-plugin-cli-4.0.0-RC10-SNAPSHOT.jar spark-impl/target/spark-plugin-impl-4.0.0-RC10-SNAPSHOT.jar spark-rest/target/spark-plugin-rest-4.0.0-RC10-SNAPSHOT.jar root@management4.critical-factor.com:/root/kudayar/subutai-4.0.0-RC10-SNAPSHOT/deploy
