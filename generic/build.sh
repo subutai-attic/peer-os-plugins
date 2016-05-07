e428b701974fa689b56d58df9991e7b1a63643c8
Ermek Abyshev <eabyshev@optimal-dynamics.com>
2016-05-07 23:01:41 +0600

    Updating version to 4.0.0-RC11

--- generic/build.sh
+++ generic/build.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-mvn clean install -Dmaven.test.skip=true && scp generic-api/target/generic-plugin-api-4.0.0-RC11-SNAPSHOT.jar generic-cli/target/generic-plugin-cli-4.0.0-RC11-SNAPSHOT.jar generic-impl/target/generic-plugin-impl-4.0.0-RC11-SNAPSHOT.jar generic-rest/target/generic-plugin-rest-4.0.0-RC11-SNAPSHOT.jar root@management4.critical-factor.com:/root/ermek/subutai-4.0.0-RC11-SNAPSHOT/deploy
+mvn clean install -Dmaven.test.skip=true && scp generic-api/target/generic-plugin-api-4.0.0-RC11.jar generic-cli/target/generic-plugin-cli-4.0.0-RC11.jar generic-impl/target/generic-plugin-impl-4.0.0-RC11.jar generic-rest/target/generic-plugin-rest-4.0.0-RC11.jar root@management4.critical-factor.com:/root/ermek/subutai-4.0.0-RC11/deploy
