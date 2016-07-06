1d7ebd4bfe6485c6362817093d02647edee2dd96
Ermek Abyshev <eabyshev@optimal-dynamics.com>
2016-07-06 17:39:02 +0600

    Releasing 4.0.0-RC14 version.

--- spark/build.sh
+++ spark/build.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-mvn clean install && scp spark-api/target/spark-plugin-api-4.0.0-RC14-SNAPSHOT.jar spark-cli/target/spark-plugin-cli-4.0.0-RC14-SNAPSHOT.jar spark-impl/target/spark-plugin-impl-4.0.0-RC14-SNAPSHOT.jar spark-rest/target/spark-plugin-rest-4.0.0-RC14-SNAPSHOT.jar root@management4.critical-factor.com:/root/kudayar/subutai-4.0.0-RC14-SNAPSHOT/deploy
+mvn clean install && scp spark-api/target/spark-plugin-api-4.0.0-RC14.jar spark-cli/target/spark-plugin-cli-4.0.0-RC14.jar spark-impl/target/spark-plugin-impl-4.0.0-RC14.jar spark-rest/target/spark-plugin-rest-4.0.0-RC14.jar root@management4.critical-factor.com:/root/kudayar/subutai-4.0.0-RC14/deploy
