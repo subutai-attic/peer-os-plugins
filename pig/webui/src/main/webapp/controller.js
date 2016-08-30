17b87d0ca1e3c84ed9cd9db1c61b4ce429d82878
Ermek Abyshev <eabyshev@optimal-dynamics.com>
2016-08-30 11:08:36 +0600

    *[Pig] - Changed text in remove node popup. #85

--- pig/webui/src/main/webapp/controller.js
+++ pig/webui/src/main/webapp/controller.js
@@ -158,7 +158,7 @@ function PigCtrl($scope, pigSrv, SweetAlert, DTOptionsBuilder, DTColumnDefBuilde
 		if(vm.currentCluster.clusterName === undefined) return;
 		SweetAlert.swal({
 			title: "Are you sure?",
-			text: "Your will not be able to recover this node!",
+			text: "This operation removes the Pig node from the cluster, and does not delete the container itself.",
 			type: "warning",
 			showCancelButton: true,
 			confirmButtonColor: "#ff3f3c",
@@ -171,7 +171,7 @@ function PigCtrl($scope, pigSrv, SweetAlert, DTOptionsBuilder, DTColumnDefBuilde
 		function (isConfirm) {
 			if (isConfirm) {
 				pigSrv.deleteNode(vm.currentCluster.clusterName, nodeId).success(function (data) {
-					SweetAlert.swal("Deleted!", "Node has been deleted.", "success");
+					SweetAlert.swal("Deleted!", "Node has been removed.", "success");
 					getClustersInfo(vm.currentCluster.clusterName);
 				});
 			}
